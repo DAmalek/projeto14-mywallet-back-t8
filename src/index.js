6c75c2a52ca8d693e0713ebd79f8dfec2b0acb91
Daniel Abdel <danielmalek101@gmail.com>
2022-11-20 10:21:53 -0300

    sing-up implementado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import { MongoClient } from "mongodb";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import joi from "joi";
+
+const userSchema = joi.object({
+  nome: joi.string().required().min(3).max(100),
+  email: joi.string().email().required(),
+  senha: joi.string().required(),
+});
 
 const app = express();
 
@@ -9,8 +17,42 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-const mongoClient = new MongoClient()
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
-app.listen(4000 , ()=> {
-    console.log('server rodando na porta 4000')
-})
+try {
+  await mongoClient.connect();
+} catch (err) {
+  console.log(err);
+}
+
+const db = mongoClient.db("projeto14");
+const userCollection = db.collection("users");
+
+app.post("/sign-up", async (req, res) => {
+  const user = req.body;
+
+  try {
+    const userExists = await userCollection.findOne({ email: user.email });
+    if (userExists) {
+      return res.status(409).send({ message: "conta já cadastrada" });
+    }
+
+    const { error } = userSchema.validate(user, { abortEarly: false });
+
+    if (error) {
+      const errors = error.details.map((detail) => detail.message);
+      return res.status(400).send(errors);
+    }
+
+    const hashPassword = bcrypt.hashSync(user.senha, 10);
+
+    await userCollection.insertOne({ ...user, senha: hashPassword });
+    res.sendStatus(201);
+  } catch (err) {
+    res.sendStatus(500)
+  }
+});
+
+app.listen(4000, () => {
+  console.log("server rodando na porta 4000");
+});
